2693d02f72f8d98cc5e598d1633ae31dcaa30701
Yash Vora <majinsparks@gmail.com>
2018-11-17 14:32:24 +0530

    added links to student on navbar

--- flaskapp.py
+++ flaskapp.py
@@ -112,6 +112,7 @@ def teachers():
         teacher_list = query_db("SELECT * FROM TEACHER")
         return render_template("/teachers_info.html", id=ID, teacher_list = teacher_list)
 
+
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
     app.run(host="0.0.0.0", port=5000, debug=True)
