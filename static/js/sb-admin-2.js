231df4c9031abb9490a1782913d9029aca263bae
Yash Vora <majinsparks@gmail.com>
2018-11-23 16:23:29 +0530

    graph example in teacher_info file

--- static/js/sb-admin-2.js
+++ static/js/sb-admin-2.js
@@ -1,42 +1,110 @@
 $(function() {
-    $('#side-menu').metisMenu();
+  // We use an inline data source in the example, usually data would
+  // be fetched from a server
+  var data = [],
+    totalPoints = 300;
+
+  function getRandomData() {
+    if (data.length > 0)
+      data = data.slice(1);
+    // Do a random walk
+    while (data.length < totalPoints) {
+      var prev = data.length > 0 ? data[data.length - 1] : 50,
+        y = prev + Math.random() * 10 - 5;
+      if (y < 0) {
+        y = 0;
+      } else if (y > 100) {
+        y = 100;
+      }
+      data.push(y);
+    }
+    // Zip the generated y values with the x values
+    var res = [];
+    for (var i = 0; i < data.length; ++i) {
+      res.push([i, data[i]])
+    }
+    return res;
+  }
+  // Set up the control widget
+  var updateInterval = 30;
+  $("#updateInterval").val(updateInterval).change(function() {
+    var v = $(this).val();
+    if (v && !isNaN(+v)) {
+      updateInterval = +v;
+      if (updateInterval < 1) {
+        updateInterval = 1;
+      } else if (updateInterval > 2000) {
+        updateInterval = 2000;
+      }
+      $(this).val("" + updateInterval);
+    }
+  });
+  var plot = $.plot("#placeholder", [getRandomData()], {
+    series: {
+      shadowSize: 0 // Drawing is faster without shadows
+    },
+    yaxis: {
+      min: 0,
+      max: 100
+    },
+    xaxis: {
+      show: false
+    }
+  });
+
+  function update() {
+    plot.setData([getRandomData()]);
+    // Since the axes don't change, we don't need to call plot.setupGrid()
+    plot.draw();
+    setTimeout(update, updateInterval);
+  }
+  update();
+  // Add the Flot version string to the footer
+  $("#footer").prepend("Flot " + $.plot.version + " &ndash; ");
+});
+
+
+
+
+$(function() {
+  $('#side-menu').metisMenu();
 });
 
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
 $(function() {
-    $(window).bind("load resize", function() {
-        var topOffset = 50;
-        var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
-        if (width < 768) {
-            $('div.navbar-collapse').addClass('collapse');
-            topOffset = 100; // 2-row-menu
-        } else {
-            $('div.navbar-collapse').removeClass('collapse');
-        }
-
-        var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
-        height = height - topOffset;
-        if (height < 1) height = 1;
-        if (height > topOffset) {
-            $("#page-wrapper").css("min-height", (height) + "px");
-        }
-    });
-
-    var url = window.location;
-    // var element = $('ul.nav a').filter(function() {
-    //     return this.href == url;
-    // }).addClass('active').parent().parent().addClass('in').parent();
-    var element = $('ul.nav a').filter(function() {
-        return this.href == url;
-    }).addClass('active').parent();
-
-    while (true) {
-        if (element.is('li')) {
-            element = element.parent().addClass('in').parent();
-        } else {
-            break;
-        }
+  $(window).bind("load resize", function() {
+    var topOffset = 50;
+    var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+    if (width < 768) {
+      $('div.navbar-collapse').addClass('collapse');
+      topOffset = 100; // 2-row-menu
+    } else {
+      $('div.navbar-collapse').removeClass('collapse');
     }
+
+    var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+    height = height - topOffset;
+    if (height < 1) height = 1;
+    if (height > topOffset) {
+      $("#page-wrapper").css("min-height", (height) + "px");
+    }
+  });
+
+  var url = window.location;
+  // var element = $('ul.nav a').filter(function() {
+  //     return this.href == url;
+  // }).addClass('active').parent().parent().addClass('in').parent();
+  var element = $('ul.nav a').filter(function() {
+    return this.href == url;
+  }).addClass('active').parent();
+
+  while (true) {
+    if (element.is('li')) {
+      element = element.parent().addClass('in').parent();
+    } else {
+      break;
+    }
+  }
 });
