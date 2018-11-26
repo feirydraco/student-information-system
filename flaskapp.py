4b03537e853a3a31202ac8553cd00eed6914601d
Yash Vora <majinsparks@gmail.com>
2018-11-26 09:19:38 +0530

    added Branch.

--- flaskapp.py
+++ flaskapp.py
@@ -195,6 +195,15 @@ def courses():
         entry_list = query_db("SELECT * FROM Courses")
         return render_template("courses.html", id=ID, entry_list=entry_list)
 
+@app.route("/branch")
+def branch():
+    global ID
+    if not session.get('logged_in'):
+        return login()
+    else:
+        entry_list = query_db("SELECT * FROM Branch")
+        return render_template("branch.html", id=ID, entry_list=entry_list)
+
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
     app.run(host="0.0.0.0", port=5000, debug=True)
